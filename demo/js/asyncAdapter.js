55e7e9d0fdb5e6181d5ad07e70754f5eb3fdf923
Geoff Davis <geoffdavis92@gmail.com>
2017-12-18 15:36:14 -0600

    update asyncAdapter with if/else instead of ternary

--- demo/js/asyncAdapter.js
+++ demo/js/asyncAdapter.js
@@ -1,22 +1,26 @@
 var asyncAdapter = (function () {
 'use strict';
 
-const asyncAdapter = (fn, ...params) => new Promise((resolve, reject) => {
-    const safeParams = params.length > 0
-        ? params
-        : fn.length === 1
-            ? new Array(fn.length).fill(null)
-            : new Array(fn.length - 1).fill(null);
-    try {
-        (fn.length > 1 &&
-            fn.length !== safeParams.length &&
-            fn(...safeParams, resolve)) ||
-            resolve(fn(...safeParams));
-    }
-    catch (e) {
-        reject(e);
-    }
-});
+const asyncAdapter = (fn, ...params) => {
+    return new Promise((resolve, reject) => {
+        const safeParams = params.length > 0
+            ? params
+            : fn.length === 1
+                ? new Array(fn.length).fill(null)
+                : new Array(fn.length - 1).fill(null);
+        try {
+            if (fn.length > 1 && fn.length !== safeParams.length) {
+                fn(...safeParams, resolve);
+            }
+            else {
+                resolve(fn(...safeParams));
+            }
+        }
+        catch (e) {
+            reject(new Error(e));
+        }
+    });
+};
 
 return asyncAdapter;
 
